3bb0122dc67b5287300b072818955b372f1e36f6
nandah4 <nyustira@gmail.com>
2024-09-11 07:17:49 +0700

    add biodata Ananda

--- biodataAnanda.java
+++ biodataAnanda.java
@@ -5,6 +5,9 @@ public class biodataAnanda {
         System.out.println("===============================================");
         System.out.println("");
         System.out.println("Nama \t : " + "Ananda Priya Yustira");
-        System.out.println("NIM \t : " + "Ananda Priya Yustira");
+        System.out.println("NIM \t : " + "244107020131");
+        System.out.println("Kelas \t : " + "TI-1C");
+        System.out.println("Alamat \t : " + "Jl. Bukit Dieng M-9A");
+        System.out.println("Jenis Kelamin \t : " + "Laki-Laki");
     }
 }
