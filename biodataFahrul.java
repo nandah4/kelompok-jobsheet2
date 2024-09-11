1137697b9e19fad2c9e044e3b3e7b6773b7d302a
nandah4 <nyustira@gmail.com>
2024-09-11 08:25:28 +0700

    feat: biodata fahrul

---
+++ biodataFahrul.java
@@ -0,0 +1,17 @@
+/*
+ * Biodata Fahrul
+ */
+
+public class biodataFahrul {
+    public static void main(String[] args) {
+        System.out.println("===============================================");
+        System.out.println("\t\t BIODATA DIRI \t\t");
+        System.out.println("===============================================");
+        System.out.println("");
+        System.out.println("Nama \t : " + "Fahrul Khoiruzaki");
+        System.out.println("NIM \t : " + "244107020234");
+        System.out.println("Kelas \t : " + "TI-1C");
+        System.out.println("Alamat \t : " + "Dusun Banyuurip Desa Sonoageng Kecamatan Prambon Kabupaten Nganjuk");
+        System.out.println("Jenis Kelamin \t : " + "Laki Laki");
+    }
+}
